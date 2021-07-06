39037d587fc1344380254b0a662b8c3e49fe861f
salahtaha97 <sal97taha@gmail.com>
2021-07-06 11:58:21 +0300

    exam

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict'
+
+let movieList = [
+    Action,
+        Adventure,
+        Animation,
+         Comedy,
+         Detective,
+         Fantasy,
+         History,
+         Horror,
+         Musical,
+         Pirate,
+         Romantic,
+         SCI-FI,
+         War,
+         Western,
+]
+
+
+
+function moviesName (movieName, movieCatogry, movieIssue){
+    this.name = movieName,
+    this.catogry = movieCatogry,
+    this.year= movieIssue
+}
+
+
+new moviesName = moviesName;
+moviesName.prototype,render = function(){
+    for (let i  = 0; i  < array.length; i ++) {
+    
+        
+    }
+}
+this.PushManager(moviesName);
+render();
+
